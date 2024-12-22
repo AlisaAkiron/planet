4cd97d866fe74896a62fde72ef4ebbe8062f251f
alisa <alisa@alisaqaq.moe>
2024-12-22 16:50:53 +0800

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import cfg from '@/config'
 
 import '@/styles/index.css'
 
+import { FC } from 'react'
 import { PublicEnvScript } from 'next-runtime-env'
 
 import { Providers } from '@/components/providers'
@@ -15,12 +16,40 @@ const RootLayout: FCC = ({ children }) => {
   return (
     <html lang="zh" suppressHydrationWarning>
       <head>
+        <WebsiteIcons />
         <PublicEnvScript />
       </head>
       <body className="font-sans antialiased">
         <Providers>{children}</Providers>
       </body>
     </html>
+  )
+}
+
+const WebsiteIcons: FC = () => {
+  return (
+    <>
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
   )
 }
 
