98d86988ad3545ffa6cb844a77911dd77d973eaf
alisa <alisa@alisaqaq.moe>
2025-02-23 23:57:27 +0800

    fix: use motion to replace framer-motion

--- src/components/animation/text/Typewriter.tsx
+++ src/components/animation/text/Typewriter.tsx
@@ -1,7 +1,7 @@
 'use client'
 
+import { motion } from 'motion/react'
 import React, { createElement } from 'react'
-import { motion } from 'framer-motion'
 import type { FC } from 'react'
 
 import { TextUpTransition } from '../transition'
