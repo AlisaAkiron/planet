86540786f55b49ed2d3dcbb5bc44a7a081769305
alisa <alisa@alisaqaq.moe>
2025-10-16 00:18:14 +0800

    fix: social link button trigger zone issue

--- src/components/ui/button/SocialLink.tsx
+++ src/components/ui/button/SocialLink.tsx
@@ -16,28 +16,31 @@ export const SocialLink = ({
   invert,
 }: SocialLinkProps) => {
   return (
-    <div className="tooltip rounded-full size-[36px]">
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer"
+      className="tooltip rounded-full size-[36px]"
+    >
       <div className="tooltip-content">
         <p>{tooltip}</p>
       </div>
-      <button
+      <span
         className={cn(
           'rounded-full w-full h-full flex items-center justify-center',
           'transform duration-300 ease-in-out hover:-translate-y-1 hover:scale-110',
         )}
         style={{ backgroundColor: color }}
       >
-        <a href={url} target="_blank" rel="noreferrer">
-          <img
-            className={cn('size-[21px]', invert && 'invert')}
-            width={24}
-            height={24}
-            src={`/icons/${icon}.svg`}
-            alt={icon}
-            style={{ color }}
-          />
-        </a>
-      </button>
-    </div>
+        <img
+          className={cn('size-[21px]', invert && 'invert')}
+          width={24}
+          height={24}
+          src={`/icons/${icon}.svg`}
+          alt={icon}
+          style={{ color }}
+        />
+      </span>
+    </a>
   )
 }
